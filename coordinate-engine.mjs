abb4e55b41044ddc4f255bca31ef504c340ed449
qjustinq <justinchen0987@gmail.com>
2026-09-11 16:15:31 -0400

    Added xFromYards function with pixel per yards

--- coordinate-engine.mjs
+++ coordinate-engine.mjs
@@ -1,7 +1,14 @@
+const PIXELS_PER_YARD = 10;
+
 function yardsFromOwnEndZone(yardline100){
     return 100- yardline100
 }
 
-let test = yardsFromOwnEndZone(2)
+function xFromYards(yardsFromOwnGoal){
+
+    return (10 + yardsFromOwnGoal) * PIXELS_PER_YARD
+}
+
+let test = xFromYards(52)
 console.log(test);
 
