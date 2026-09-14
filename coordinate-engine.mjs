8f6ee82d7f66a3d5d5573e2210e12554cb62a47b
qjustinq <justinchen0987@gmail.com>
2026-09-14 01:30:12 -0400

    Added a get EndPoint function

--- coordinate-engine.mjs
+++ coordinate-engine.mjs
@@ -1,6 +1,6 @@
 const PIXELS_PER_YARD = 10;
 const Y_BAND = {left : 130, middle : 260, right: 390};
-const play = {yardline_100 : 48 , pass_location : "right" }
+const play = {yardline_100 : 48 , pass_location : "right", air_yards : -4 , yards_gained: 4}
 
 function yardsFromOwnEndZone(yardline100){
     return 100- yardline100
@@ -10,7 +10,7 @@ function xFromYards(yardsFromOwnGoal){
     return (10 + yardsFromOwnGoal) * PIXELS_PER_YARD
 }
 
-function yFromLocation(location, str){
+function yFromLocation(location){
     return Y_BAND[location]
 }
 
@@ -18,6 +18,24 @@ function getLOS(play){
     return {x : xFromYards(yardsFromOwnEndZone(play.yardline_100)), y :  yFromLocation(play.pass_location) }
 }
 
-let test = getLOS(play)
+function getCatchPoint(play){
+    if (play.air_yards == null || undefined){
+        return null
+    } else {
+        return {x: xFromYards(yardsFromOwnEndZone(play.yardline_100) + play.air_yards) , y: yFromLocation(play.pass_location)  } 
+    }
+}
+
+function getEndPoint(play){
+    if (play.yards_gained == null || undefined){
+        return null
+    } else {
+        return {x: xFromYards(yardsFromOwnEndZone(play.yardline_100) + play.yards_gained) ,
+                y: yFromLocation(play.pass_location)
+        }
+    }
+}
+    
+let test = getEndPoint(play)
 console.log(test);
 
