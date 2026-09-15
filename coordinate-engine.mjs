27b11e62a6b510f1d303fcd80802ab75d914c64b
qjustinq <justinchen0987@gmail.com>
2026-09-15 01:54:25 -0400

    Did some test cases and some didnt work like sack and imcomplete with work on it next day

--- coordinate-engine.mjs
+++ coordinate-engine.mjs
@@ -1,6 +1,9 @@
+import plays from './test-plays.json' with {type: 'json'};
+
 const PIXELS_PER_YARD = 10;
 const Y_BAND = {left : 130, middle : 260, right: 390};
 const play = {play_type: "run", yardline_100 : 72 , run_location : "left", pass_location : null, air_yards : null , yards_gained: 15}
+const play1 = {play_type: "pass", yardline_100 : 34 , run_location : "null", pass_location : "right", air_yards : 24 , yards_gained: 25}
 
 function yardsFromOwnEndZone(yardline100){
     return 100- yardline100
@@ -27,7 +30,7 @@ function getLOS(play){
 }
 
 function getCatchPoint(play){
-    if (play.air_yards === null || play.yards_gained === undefined){
+    if (play.air_yards === null || play.air_yards === undefined){
         return null
     } else {
         return {x: xFromYards(yardsFromOwnEndZone(play.yardline_100) + play.air_yards) , y: yFromLocation(getLocation(play))  } 
@@ -41,10 +44,13 @@ function getEndPoint(play){
         return {x: xFromYards(yardsFromOwnEndZone(play.yardline_100) + play.yards_gained) ,
                 y: yFromLocation(getLocation(play))
         }
-    }
+    }   
 }
-    
 
-let test = getEndPoint(play)
-console.log(test);
+function playToCoordinates(play){
+    return {LOS: getLOS(play), Catch: getCatchPoint(play), End: getEndPoint(play), Meta: {play_type : play.play_type, air_yards: play.air_yards, yards_gained : play.yards_gained} }
+}
 
+for (let i = 0; i < plays.length; i++){
+    console.log(playToCoordinates(plays[i]))
+}
