b2170a496a20f06afdaa724efdc22b0b8c1953a9
qjustinq <justinchen0987@gmail.com>
2026-09-19 23:30:46 -0400

    finished the line connecting the start to end

--- field-renderer.mjs
+++ field-renderer.mjs
@@ -8,24 +8,33 @@ function buildExampleSVG(playCoords){
     const totalWidth = 120 * PIXELS_PER_YARD
     const endZoneWidth = 10 * PIXELS_PER_YARD
     const rightEndZoneX = totalWidth - endZoneWidth
+
     const catchCircle = playCoords.Catch
         ? `<circle cx = "${playCoords.Catch.x}" cy = "${playCoords.Catch.y}" r = "8" fill = "yellow" />`
         : '';
+
+    const pathLines = playCoords.Catch
+        ? `<line x1 = "${playCoords.LOS.x}" y1 = "${playCoords.LOS.y}" x2 = "${playCoords.Catch.x}" y2 = "${playCoords.Catch.y}" stroke = "white" stroke-width = "2" />
+           <line x1 ="${playCoords.Catch.x}" y1="${playCoords.Catch.y}" x2="${playCoords.End.x}" y2="${playCoords.End.y}" stroke="white" stroke-width="2" />`
+        : `<line x1 ="${playCoords.LOS.x}" y1="${playCoords.LOS.y}" x2="${playCoords.End.x}" y2="${playCoords.End.y}" stroke="white" stroke-width="2" />`;
+
     const height = 520;
 
     return `<svg width = "${totalWidth}" height = "${height}">
             <rect x = "0" y = "0" width = "${totalWidth}" height = "${height}" fill = "green" /> 
             <rect x = "0" y = "0" width = "${endZoneWidth}" height = "${height}" fill = "blue" />
             <rect x = "${rightEndZoneX}" y = "0" width = "${endZoneWidth}" height = "${height}" fill = "blue" /> 
+            ${pathLines}
             ${catchCircle}
             <circle cx = "${playCoords.LOS.x}" cy = "${playCoords.LOS.y}" r = "8" fill = "red" />
             <circle cx = "${playCoords.End.x}" cy = "${playCoords.End.y}" r = "8" fill = "red" />
             
+
             
     </svg>`;  
 }
 
-const svgContent = buildExampleSVG(playToCoordinates(plays[3]));
+const svgContent = buildExampleSVG(playToCoordinates(plays[2]));
 const fullPage = `<html><body>${svgContent}</body></html>`;
 
 fs.writeFileSync('example.html', fullPage);
